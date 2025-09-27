53c16b3f759fbcb63e82de99115918df9e1c6d3c
SR <saodat.riskiyeva@gmail.com>
2025-09-26 20:03:12 -0400

    starting SQL Workbench

--- db-injection/data/database.js
+++ db-injection/data/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: "mySQL7!",
+  multipleStatements: true,
+});
 
 module.exports = pool;
